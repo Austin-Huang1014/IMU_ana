89a2be2cc5ba883c005202ac18b0170a89d65b2f
Austin <sdnd93612@gmail.com>
2020-08-15 20:49:39 +0800

    update2020/8/15

---
+++ duckiefloat/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/austin/IMU_ana/duckiefloat/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/austin/IMU_ana/duckiefloat/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/austin/IMU_ana/duckiefloat/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/austin/IMU_ana/duckiefloat/src:$ROS_PACKAGE_PATH"
